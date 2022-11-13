77b5718ea12a2bf4337e3e60d8bab1ae6111f47b
Roberto Ruiz <roberto@fxdigital.uk>
2022-11-13 20:01:38 +0100

    feat: add avocado theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
-import "../styles/schemes/congo.css";
+import "../styles/schemes/avocado.css";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
