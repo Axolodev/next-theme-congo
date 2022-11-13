2d796b628c9a7171fc3f28daf84dcd586a75ef8e
Roberto Ruiz <roberto@fxdigital.uk>
2022-11-13 16:46:49 +0100

    feat: add theme provider, remove mainlayout

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,2 +1,1 @@
 export * from "./Header";
-export { default as MainLayout } from "./mainLayout";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
+import { ThemeProvider } from "next-themes";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
-import { MainLayout } from "../components";
 import "../styles/globals.css";
 import "../styles/schemes/congo.css";
 
@@ -13,10 +13,21 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
+/** @TODO Add RTL support */
+
+function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="class">
+      <div className="flex flex-col h-screen px-6 m-auto text-lg leading-7 max-w-7xl bg-neutral text-neutral-900 dark:bg-neutral-800 dark:text-neutral sm:px-14 md:px-24 lg:px-32">
+        {children}
+      </div>
+    </ThemeProvider>
+  );
+}
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return <MainLayout>{getLayout(<Component {...pageProps} />)}</MainLayout>;
+  return <AppLayout>{getLayout(<Component {...pageProps} />)}</AppLayout>;
 }
