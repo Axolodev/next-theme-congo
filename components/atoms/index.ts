b7609c3ad8518b7edddbedac421407cfcdcf3c85
Roberto Ruiz <roberto@fxdigital.uk>
2022-11-21 02:10:21 +0100

    refactor: remove unnecessary code

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,5 +1,3 @@
-export { default as MoonIcon } from "./moonIcon";
-export { default as SunIcon } from "./sunIcon";
 export { default as Copyright } from "./copyright";
 export { default as Icon } from "./icon";
 export type { IconProps } from "./icon";
--- components/atoms/moonIcon.tsx
+++
@@ -1,3 +0,0 @@
-export default function MoonIcon() {
-  return <>🌙</>;
-}
